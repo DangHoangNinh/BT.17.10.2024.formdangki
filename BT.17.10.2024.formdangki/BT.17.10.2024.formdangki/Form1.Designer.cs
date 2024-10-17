fe2e53a84415e987f8d930863e34708956808b73
DangHoangNinh <danghoangninh52@gmail.com>
2024-10-17 15:39:22 +0700

    Add files via upload

---
+++ BT.17.10.2024.formdangki/BT.17.10.2024.formdangki/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BT._17._10._2024.formdangki
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            TxtName = new TextBox();
+            TxtEmail = new TextBox();
+            TxtPassword = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            BtnRegister = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            label1.Location = new Point(276, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(251, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Đăng kí tài khoản";
+            // 
+            // TxtName
+            // 
+            TxtName.Location = new Point(334, 134);
+            TxtName.Name = "TxtName";
+            TxtName.Size = new Size(224, 27);
+            TxtName.TabIndex = 1;
+            // 
+            // TxtEmail
+            // 
+            TxtEmail.Location = new Point(334, 207);
+            TxtEmail.Name = "TxtEmail";
+            TxtEmail.Size = new Size(224, 27);
+            TxtEmail.TabIndex = 2;
+            // 
+            // TxtPassword
+            // 
+            TxtPassword.Location = new Point(334, 280);
+            TxtPassword.Name = "TxtPassword";
+            TxtPassword.Size = new Size(224, 27);
+            TxtPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(220, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Tên :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(220, 210);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Email :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(220, 279);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Mật khẩu :";
+            // 
+            // BtnRegister
+            // 
+            BtnRegister.Location = new Point(358, 363);
+            BtnRegister.Name = "BtnRegister";
+            BtnRegister.Size = new Size(94, 29);
+            BtnRegister.TabIndex = 7;
+            BtnRegister.Text = "Đăng kí";
+            BtnRegister.UseVisualStyleBackColor = true;
+            BtnRegister.Click += btnRegister_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(BtnRegister);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(TxtPassword);
+            Controls.Add(TxtEmail);
+            Controls.Add(TxtName);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Formdangki";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox TxtName;
+        private TextBox TxtEmail;
+        private TextBox TxtPassword;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button BtnRegister;
+    }
+}
---
+++ BT.17.10.2024.formdangki/BT.17.10.2024.formdangki/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace BT._17._10._2024.formdangki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TxtPassword.PasswordChar = '*';
+        }
+
+        // Hàm kiểm tra định dạng Email với Regex
+        private bool IsValidEmail(string email)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$"; // Regular Expression cho email hợp lệ
+            return Regex.IsMatch(email, emailPattern);
+        }
+
+        // Hàm kiểm tra tính hợp lệ của Password với Regex
+        private bool IsValidPassword(string password)
+        {
+            string passwordPattern = @"^(?=.*[!@#$%^&*(),.?""{}|<>])[A-Za-z\d!@#$%^&*(),.?""{}|<>]{8,}$"; // Regular Expression cho mật khẩu hợp lệ
+            return Regex.IsMatch(password, passwordPattern);
+        }
+
+        // Sự kiện khi nhấn nút "Đăng ký"
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string name = TxtName.Text;
+            string email = TxtEmail.Text;
+            string password = TxtPassword.Text;
+
+            // Kiểm tra tính hợp lệ của email và password
+            if (IsValidEmail(email) && IsValidPassword(password))
+            {
+                MessageBox.Show("Đăng ký thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Email hoặc mật khẩu không hợp lệ! " +
+                    "Email phải đúng định dạng, mật khẩu tối thiểu 8 ký tự và phải bao gồm ít nhất 1 ký tự đặc biệt.",
+                    "Thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
